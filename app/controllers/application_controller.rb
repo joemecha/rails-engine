370c19cbfec5c30415b72e3d96f4cc6dd161bfa6
joemecha <jwmecha@gmail.com>
2021-05-23 16:19:49 -0500

    Complete get items for a merchant and add rescue_from in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+  # include ActionController::Helpers
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :record_bad_request
+
+
+  private 
+
+  def record_not_found
+    render plain: "404 Not Found", status: 404
+  end
+
+  def record_bad_request
+    render json: { error: '400 Bad Request' }, status: :bad_request
+  end
 end
--- spec/requests/api/v1/merchant_items/index_spec.rb
+++ spec/requests/api/v1/merchant_items/index_spec.rb
@@ -7,9 +7,8 @@ RSpec.describe "Merchant Items API" do
     create_list(:item, 3, merchant: @merchant)
   end
 
-  describe "happy path" do
+  describe "Happy path" do
     it "shows a list of a merchant's items" do
-      create_list(:merchant, 20)
       get "/api/v1/merchants/#{@merchant.id}/items"
 
       items = JSON.parse(response.body, symbolize_names: true)
@@ -33,75 +32,11 @@ RSpec.describe "Merchant Items API" do
     end
   end 
 
-
-
-
-  #   it 'sends a list of merchants 20 at a time' do
-  #     create_list(:merchant, 21)
-  #     get '/api/v1/merchants'
-  #     expect(response).to be_successful
-  #     first_merchants = JSON.parse(response.body, symbolize_names: true)
-
-  #     get '/api/v1/merchants?page=0'
-  #     expect(response).to be_successful
-  #     page_0_merchants = JSON.parse(response.body, symbolize_names: true)
-
-  #     expect(first_merchants.count).to eq(20)
-  #     expect(first_merchants).to eq(page_0_merchants)
-  #   end
-
-  #   it 'shows the second 20 merchants' do
-  #     create_list(:merchant, 40)
-  #     get '/api/v1/merchants?page=1'
-
-  #     expect(response).to be_successful
-
-  #     page_2 = JSON.parse(response.body, symbolize_names: true)
-
-  #     expect(page_2.count).to eq(20)
-  #   end
-
-  #   it 'shows first page of 50 merchants' do
-  #     create_list(:merchant, 51)
-  #     get '/api/v1/merchants?per_page=50'
-
-  #     expect(response).to be_successful
-  #     merchants = JSON.parse(response.body, symbolize_names: true)
-
-  #     expect(merchants.count).to eq(50)
-  #   end
-
-  #   it 'shows all merchants for large per_page requests' do
-  #     create_list(:merchant, 101)
-  #     get '/api/v1/merchants?per_page=100'
-
-  #     expect(response).to be_successful
-
-  #     merchants = JSON.parse(response.body, symbolize_names: true)
-
-  #     expect(merchants.count).to eq(100)
-  #   end
-
-  #   it 'shows a blank page for range of merchants that contains no data' do
-  #     get '/api/v1/merchants?page=300'
-
-  #     expect(response).to be_successful
-  #     merchants = JSON.parse(response.body, symbolize_names: true)
-
-  #     expect(merchants.count).to eq(0)
-  #   end
-  # end
-
-  # describe 'sad path' do
-  #   it 'shows page 1 if page is 0 or lower' do
-  #     get '/api/v1/merchants?page=1'
-  #     page_1 = JSON.parse(response.body, symbolize_names: true)
-
-  #     get '/api/v1/merchants?page=0'
-  #     expect(response).to be_successful
-  #     page_0 = JSON.parse(response.body, symbolize_names: true)
-
-  #     expect(page_1).to eq(page_0)
-  #   end
-  # end
+  describe "Sad path" do
+    it "Returns 404 with bad merchant id" do
+      get "/api/v1/merchants/100000000/items"
+      
+      expect(response.status).to eq 404
+    end
+  end  
 end 
