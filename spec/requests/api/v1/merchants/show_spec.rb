be1de91414bd3e20e4a6d99eec8aaa0904619910
joemecha <jwmecha@gmail.com>
2021-05-23 18:06:44 -0500

    Restore Travis config and correct failing test in Merchants Show and rubocop formatting

--- spec/requests/api/v1/merchants/show_spec.rb
+++ spec/requests/api/v1/merchants/show_spec.rb
@@ -9,15 +9,15 @@ RSpec.describe 'Merchants API' do
     it 'gets one merchant by its id' do
       get "/api/v1/merchants/#{@merchant.id}"
 
-      merchant = JSON.parse(response.body, symbolize_names: true)
+      merchant_info = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to be_successful
+      expect(merchant_info).to have_key(:id)
+      expect(merchant_info[:id].to_i).to eq(@merchant.id)
 
-      expect(merchant[:data]).to have_key(:id)
-      expect(merchant[:data][:id].to_i).to eq(@merchant.id)
-
-      expect(merchant[:data][:attributes]).to have_key(:name)
-      expect(merchant[:data][:attributes][:name]).to be_a(String)
+      expect(merchant_info).to have_key(:name)
+      expect(merchant_info[:name]).to be_a(String)
     end
   end
 end
+
