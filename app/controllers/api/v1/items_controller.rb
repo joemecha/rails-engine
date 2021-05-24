465ce04ae74c37913ab147309db50e97a45be6e5
joemecha <jwmecha@gmail.com>
2021-05-24 17:47:16 -0500

    Add modules for json error status message handling

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,12 +1,10 @@
 class API::V1::ItemsController < API::APIController
   def index
-    page = params[:page].to_i || 0
-    if params[:per_page].present?
-      results_limit = params[:per_page].to_i
-      items = Item.limit(results_limit).offset(page * results_limit)
-    else
-      items = Item.limit(20).offset(page * 20)
-    end
+    page = [params.fetch(:page, 1).to_i, 1].max
+    per_page = params.fetch(:per_page, 20).to_i
+    
+    items = Item.limit(per_page).offset((page - 1) * per_page)
+    
     render json: items
   end
 
