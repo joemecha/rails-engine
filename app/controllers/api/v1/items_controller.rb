e4163645a42c648eadfcc58e9e845376a84c14c2
joemecha <jwmecha@gmail.com>
2021-05-24 17:53:09 -0500

    Rubocop formatting

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,9 +2,9 @@ class API::V1::ItemsController < API::APIController
   def index
     page = [params.fetch(:page, 1).to_i, 1].max
     per_page = params.fetch(:per_page, 20).to_i
-    
+
     items = Item.limit(per_page).offset((page - 1) * per_page)
-    
+
     render json: items
   end
 
