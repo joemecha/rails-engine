682a89f6e4102cac6ec644fc3d5e0caa557f3a0f
joemecha <jwmecha@gmail.com>
2021-05-25 21:33:26 -0500

    Change search routing to use namespace not resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,22 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: %i[index show] do
+        # resources :find, controller: :search, only: :index 
         resources :items, controller: :merchant_items, only: :index
       end
 
       resources :items do 
+        resources :find_all, controller: :items_search, only: :index
         resources :merchant, controller: :items_merchant, only: :index
-      end 
+      end
+
+      namespace :merchants do
+        resources :find, controller: :search, only: :index
+      end
+
+      namespace :items do
+        resources :find_all, controller: :search, only: :index
+      end
     end
   end
 end
